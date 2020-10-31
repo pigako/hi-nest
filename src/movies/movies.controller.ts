934892d4ee2c41d8f54a34a617fb6e2bb8798109
Toby Baek <50404311+pigako@users.noreply.github.com>
2020-10-31 18:11:03 +0900

    learn more service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,23 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { Movie } from './entities/movie.entity';
-import { MoviesService } from './movies.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import { Movie } from "./entities/movie.entity";
+import { MoviesService } from "./movies.service";
 
-@Controller('movies')
+@Controller("movies")
 export class MoviesController {
-    constructor(private readonly movieService: MoviesService) { }
+    constructor(private readonly movieService: MoviesService) {}
 
     @Get()
     getAllMovies(): Movie[] {
-        return this.movieService.getAll()
+        return this.movieService.getAll();
     }
 
-    @Get('search')
-    search(@Query('year') searchingYear: string) {
-        return ''
+    @Get("search")
+    search(@Query("year") searchingYear: string) {
+        return "";
     }
 
-    @Get('/:id')
-    getOneMovies(@Param('id') id : string): Movie {
+    @Get("/:id")
+    getOneMovies(@Param("id") id: string): Movie {
         return this.movieService.getOne(id);
     }
 
@@ -26,16 +26,13 @@ export class MoviesController {
         return this.movieService.create(movieData);
     }
 
-    @Delete('/:id')
-    removeMovie(@Param('id') id : string) {
-        return `This will remove a movie with the id ${id}`
+    @Delete("/:id")
+    removeMovie(@Param("id") id: string) {
+        return this.movieService.deleteOne(id);
     }
 
-    @Patch('/:id')
-    patchMovie(@Param('id') id: string, @Body() updateData) {
-        return {
-            updatedMovie: id,
-            ...updateData
-        }
+    @Patch("/:id")
+    patchMovie(@Param("id") id: string, @Body() updateData) {
+        return this.movieService.update(id, updateData);
     }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,27 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { Movie } from './entities/movie.entity';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Movie } from "./entities/movie.entity";
 
 @Injectable()
 export class MoviesService {
     private movies: Movie[] = [];
 
     getAll(): Movie[] {
-        return this.movies
+        return this.movies;
     }
 
     getOne(id: string): Movie {
-        return this.movies.find((movie) => movie.id === parseInt(id));
+        const movie = this.movies.find((movie) => movie.id === parseInt(id));
+        if (!movie) {
+            throw new NotFoundException("Not Exits Movie's id");
+        }
+
+        return movie;
     }
 
-    deleteOne(id: string): boolean {
-        this.movies.filter((movie) => movie.id !== parseInt(id));
-        return true;
+    deleteOne(id: string) {
+        this.getOne(id);
+        this.movies = this.movies.filter((movie) => movie.id !== parseInt(id));
     }
 
     create(movieData) {
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData
-        })
+        });
+    }
+
+    update(id: string, updateData) {
+        const movie = this.getOne(id);
+        this.deleteOne(id);
+        this.movies.push({ ...movie, ...updateData });
     }
 }
